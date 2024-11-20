9c813d5b34544c9c0b6bf82f31169b0b97fad7bc
RTAkland <me@rtast.cn>
2024-11-20 22:29:46 +0800

    fix: fix bug and upgrade ide version to 2024.3

--- src/main/kotlin/com/github/aimerny/mcdrdev/action/CreatePluginAction.kt
+++ src/main/kotlin/com/github/aimerny/mcdrdev/action/CreatePluginAction.kt
@@ -1,15 +1,13 @@
 package com.github.aimerny.mcdrdev.action
 
 import com.github.aimerny.mcdrdev.util.invokeLater
-import com.github.aimerny.mcdrdev.view.CreatePluginDialog
 import com.github.aimerny.mcdrdev.util.runWrite
+import com.github.aimerny.mcdrdev.view.CreatePluginDialog
 import com.intellij.ide.projectView.ProjectView
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.application.WriteAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.util.IconLoader
@@ -30,6 +28,10 @@ class CreatePluginAction : AnAction() {
         }
     }
 
+    override fun getActionUpdateThread(): ActionUpdateThread {
+        return ActionUpdateThread.BGT
+    }
+
     override fun update(e: AnActionEvent) {
         // 确保只有在选中文件夹时才启用 action
         val virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE)
@@ -42,13 +44,12 @@ class CreatePluginAction : AnAction() {
             val pluginId = dialog.getPluginId()
             val pluginName = dialog.getPluginName()
             val author = dialog.getPluginAuthor()
-            // create mcdrplugin files
+            val version = dialog.getPluginVersion()
             try {
-                // plugin src
                 runWrite {
-                    writePluginFiles(vf, pluginId, pluginName, author, project)
+                    writePluginFiles(vf, pluginId, version, pluginName, author, project)
                 }
-//                Messages.showMessageDialog(project, "PluginCreated", "Plugin Created", Messages.getInformationIcon())
+                Messages.showMessageDialog(project, "PluginCreated", "Plugin Created", Messages.getInformationIcon())
             } catch (ex: Exception) {
                 Messages.showErrorDialog(project, "Failed to create MCDR Plugin: ${ex.message}", "Error")
             }
@@ -58,27 +59,27 @@ class CreatePluginAction : AnAction() {
     private fun writePluginFiles(
         vf: VirtualFile,
         pluginId: String,
+        version: String,
         pluginName: String,
         author: String,
         project: Project
     ) {
-        val pluginDir = vf.createChildDirectory(this, pluginId)
-        val srcDir = pluginDir.createChildDirectory(null, pluginId)
+//        val pluginDir = vf.createChildDirectory(this, pluginId)
+        val srcDir = vf.createChildDirectory(null, pluginId)
         srcDir.createChildData(null, "__init__.py")
-        // plugin meta file
-        val pluginMetaJson = pluginDir.createChildData(null, "mcdreforged.plugin.json")
-        val content = pluginMetaContent(pluginId, pluginName, author)
+        val pluginMetaJson = srcDir.createChildData(null, "mcdreforged.plugin.json")
+        val content = pluginMetaContent(pluginId, version, pluginName, author)
         VfsUtil.saveText(pluginMetaJson, content)
-        pluginDir.refresh(false, true)
+        srcDir.refresh(false, true)
         invokeLater {
-            ProjectView.getInstance(project).select(null, pluginDir, false)
+            ProjectView.getInstance(project).select(null, srcDir, false)
         }
     }
 
-    private fun pluginMetaContent(pluginId: String, pluginName: String, author: String): String {
+    private fun pluginMetaContent(pluginId: String, version: String, pluginName: String, author: String): String {
         return """{
                  |  "id": "$pluginId",
-                 |  "version": "1.0.0",
+                 |  "version": "$version",
                  |  "name": "$pluginName",
                  |  "description": {
                  |    "en_us": "Your Plugin Description",
--- src/main/kotlin/com/github/aimerny/mcdrdev/view/CreatePluginDialog.kt
+++ src/main/kotlin/com/github/aimerny/mcdrdev/view/CreatePluginDialog.kt
@@ -1,17 +1,15 @@
 package com.github.aimerny.mcdrdev.view
 
 import com.intellij.openapi.ui.DialogWrapper
-import com.intellij.ui.components.JBTextField
-import com.intellij.ui.dsl.builder.RowLayout
 import com.intellij.ui.dsl.builder.panel
 import javax.swing.JComponent
 import javax.swing.JTextField
 
-class CreatePluginDialog: DialogWrapper(true) {
+class CreatePluginDialog : DialogWrapper(true) {
 
     private var pluginIdField = JTextField()
     private var pluginNameField = JTextField()
-//    private var pluginVersionField = JTextField()
+    private var pluginVersionField = JTextField()
     private var pluginAuthorField = JTextField()
 
     init {
@@ -23,13 +21,13 @@ class CreatePluginDialog: DialogWrapper(true) {
         return panel {
             row("Plugin ID:") { pluginIdField = textField().component }
             row("Plugin Name:") { pluginNameField = textField().component }
-//            row("Version:") { pluginVersionField = textField().component }
+            row("Version:") { pluginVersionField = textField().component }
             row("Author:") { pluginAuthorField = textField().component }
         }
     }
 
     fun getPluginId(): String = pluginIdField.text
     fun getPluginName(): String = pluginNameField.text
-//    fun getPluginVersion(): String = pluginVersionField.text
+    fun getPluginVersion(): String = pluginVersionField.text
     fun getPluginAuthor(): String = pluginAuthorField.text
 }
